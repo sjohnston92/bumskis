18917af146e89a7c2096816cf4825df30e156f14
Simon Johnston <simonjohnston@Simons-MacBook-Pro.local>
2020-11-12 12:41:02 -0700

    Search added. Logos designed and added

--- client/src/Components/Lander.js
+++ client/src/Components/Lander.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import {Button, Container} from 'react-bootstrap';
+import React,{useState} from 'react';
+import {Button, Container,Form} from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
+import styled from "styled-components"
 
 
-const Lander = () => (
-  <div>
-  <Container>
-  <h1 className="landing">Welcome to (Project-Name)</h1>
-  <br />
-  <br />
-  <Button  href="/login"variant="info" block>Login</Button>
+const Lander = () => {
+  const [query, setQuery] = useState("");
+  const history = useHistory();
 
-  <Button href="/register" variant="success" block>Sign Up</Button>
-  </Container>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`/search?name=${query}`);
+  };
+
+  return(
+  <div >
+    <div className="landing">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label style={{ color: "black", width: "100%" }}>
+            <StyledSearchText>
+              Buy. Sell. Trade. & Rent. 
+            </StyledSearchText>
+          </Form.Label>
+          <MainInput
+            className="form"
+            autoFocus
+            placeholder="E.g. ‘Jackson Hole’, ‘Breckenridge’, ‘Killington’"
+            name="query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      </div>
   </div>
-)
+  )
+}
+
+const MainInput = styled(Form.Control)`
+  padding: 1.5rem;
+  border-radius: 15px 50px;
+`;
+
+
+
+
+
+const StyledSearchText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 100%;
+  font-family: 'Noto Sans', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 44px;
+  margin-bottom: 1rem;
+  padding: 0 0.5rem;
+`;
 
 export default Lander;
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React, {useContext}from 'react';
-import {Nav,Navbar, NavDropdown} from 'react-bootstrap';
+import {Nav,Navbar, NavDropdown,Col,Row} from 'react-bootstrap';
 import {AuthContext} from '../providers/AuthProvider'
 import Logo from './images/1x/bumskiLogo.png'
 import {Link,useHistory} from "react-router-dom"
@@ -38,25 +38,30 @@ const getRightNav = () => {
 
 
 return (
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="justify-content-end">
-<Navbar.Brand style={{paddingRight:"65%",paddingLeft: "5px"}}href="#home"><img style={{width:"100px"}}src={Logo}/></Navbar.Brand>
+<Navbar collapseOnSelect expand="lg">
+
+<Col>
+<Navbar.Brand href="/"><img style={{width:"200px"}}src={Logo}/></Navbar.Brand>
+</Col>
+<Col>
 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 <Navbar.Collapse id="responsive-navbar-nav">
 <Nav  activeKey="/home">
 <Nav.Item>
-  {user && <Nav.Link href="/home">Home</Nav.Link>}
+  <Nav.Link href="/home">Home</Nav.Link>
 </Nav.Item>
 <Nav.Item>
-  {user && <Nav.Link href="/about">About</Nav.Link>}
+  <Nav.Link href="/about">About</Nav.Link>
 </Nav.Item>
 <Nav.Item>
-  {user && <Nav.Link href="/map">Map</Nav.Link>}
+  <Nav.Link href="/map">Map</Nav.Link>
 </Nav.Item>
 <NavDropdown title="BumSkis Profile" id="basic-nav-dropdown">
 {getRightNav()}
 </NavDropdown>
 </Nav>
 </Navbar.Collapse>
+</Col>
 </Navbar>
 
 )
