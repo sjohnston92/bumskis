096d94293032d0cb0fee521ebc516b94af67a9f5
Zane Newsom <69982425+ZaneN8@users.noreply.github.com>
2020-12-09 09:31:13 -0700

    Zane (#5)
    * first upload for me
    
    * CRUD added to post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,4 +4,39 @@ class Api::PostsController < ApplicationController
     render json: Post.all
   end
 
+  def show
+    render json: @post
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post.errors, status: 422
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: 422
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:sample).permit(:user_id, :size, :location, :title, :price, :sold, :image)
+  end
+
 end
