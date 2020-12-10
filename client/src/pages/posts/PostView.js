43d2ee734e3ed99f056ececd096884ad78928152
VidensKnowledge <69538975+VidensKnowledge@users.noreply.github.com>
2020-12-10 10:23:33 -0700

    working PostView with comments rendering (#15)

--- client/src/pages/posts/PostView.js
+++ client/src/pages/posts/PostView.js
@@ -36,7 +36,7 @@ const PostView = ({ match }) => {
       </Row>
       <Row>
         <CommentArea>
-          <Comments />
+          <Comments  post={match.params.id}/>
         </CommentArea>
       </Row>
     </Container>
@@ -51,8 +51,6 @@ const CommentArea = styled.div`
   transition: 0.3s;
   box-shadow: 0px 4px 10px 2px rgba(0, 0, 0, 0.35);
   border-radius: 30px;
-  width: 80%;
-  height: 350px;
   margin: 1rem;
 `;
 
