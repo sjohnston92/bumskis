e0a08cf59d1b7b4cb85a7e6ed2f94025db190798
VidensKnowledge <69538975+VidensKnowledge@users.noreply.github.com>
2020-12-18 12:11:28 -0700

    Vidbranch (#21)
    * Delete for comments is working
    
    * Have working PostForm

--- client/src/pages/posts/PostForm.js
+++ client/src/pages/posts/PostForm.js
@@ -1,227 +1,122 @@
-import React, { useState, useContext } from "react";
-import Axios from "axios";
-import { Formik, useFormik } from 'formik';
-import * as Yup from 'yup';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
+import {AuthContext} from "../../providers/AuthProvider"
 
-import { AuthConsumer, AuthContext } from "../../providers/AuthProvider";
+const PostForm = ({post, hide, add}) =>{
+  const auth = useContext(AuthContext);
+  const [postState, setPostState] = useState(
+    post ? {
+      user_id: auth.user.id,
+      price: post.price,
+      title: post.title,
+      location: post.location,
+      size: post.size,
+    }  :
+    {
 
-const PostForm = (props) => {
-  const [ post, setPost ]= useState('');
-  const { user } = useContext(AuthContext);
-  const [ image, setImage ] = useState('');
-  const [ sold, setSold ] = useState(false);
-  const [ price, setPrice ] = useState(0);
-  const [ title, setTitle ] = useState('');
-  const [ location, setLocation ] = useState('');
-  const [ size, setSize ] = useState('');
+      price: 0,
+      title: '',
+      location: '',
+      size: '',
+      user_id: auth.user.id,
+    }
+);
 
-  const formik = useFormik({
-      initialValues:{ 
-        title: '',
-        image: '',
-        price: '',
-        location: '',
-        size: ''},
-  })
-    const fakeConst = [];
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   addPost({ title: title, image: image, price: price, location: location, size: size, })
+  // }
 
-  const addPost = (post) => {
+    const handleChange = (e) => {
+      setPostState({ ...postState, [e.target.name]: e.target.value });
+    };
+
+    const addPost = async () => {
+      console.log(postState.title)
       debugger
-          Axios
-            .post(`/api/posts/`, post)
-            .then((res) => {
-              setPost(res.data);
-            })
-            .catch((error) => {
-              alert("error in submitting post");  
-            });
-        
-          }
+      try {
+        let res = await axios.post(`/api/posts`, postState);
+        add(res.data)
+      } catch (err) {
+      alert("ERROR: add post doesn't work")
+    }
+  }
 
-  
-  
+    const editPost = async () => {
+      try {
+        let res = await axios.put(
+          `/api/posts/${post}`
+        );
+      } catch (err) {
+      alert("ERROR: EDIT post doesnt work")
+    }
+  }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (post) {
+        editPost();
+      } else {
+        addPost()
+      }
+    }
+ 
+  // const handleChange = (e) => {
+  //   console.log("change")
+  // }
+
   return(
     <>
-     <Formik
-      initialValues={{ title: "", image: "", price: "", location: "", size: "", }}
-      onSubmit={async values => {
-        try {
-        let res = await Axios.post(`/api/posts`, values);
-          setPost(res.data);
-        } catch (err) {
-          alert(JSON.stringify(values, null, 2));
-        }
-      }}
-      // validationSchema={Yup.object().shape({
-      //   title: Yup.string()
-      //     .title()
-      //     .required("Required"),
-      //   price: Yup.string()
-      //     .price()
-      //     .required("Required"),
-      //   location: Yup.string()
-      //     .location()
-      //     .required("Where are you?"),
-      //   size: Yup.string()
-      //     .size()
-      //     .required("What size is it?")
-      // })}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          dirty,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          handleReset
-        } = props;
-        return (
-          <form onSubmit={handleSubmit}>
-            <label style={{ display: "block" }}>
-              Title
-            </label>
-            <input
-              id="title"
-              placeholder="What are you posting?"
-              type="text"
-              value={values.title}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.title && touched.title
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-                        {errors.title && touched.title && (
-              <div className="input-feedback">{errors.title}</div>
-            )}
-            <input
-              id="image"
-              placeholder="Enter your image URL (will be changed)"
-              type="text"
-              value={values.image}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.image && touched.image
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-                        {errors.image && touched.image && (
-              <div className="input-feedback">{errors.image}</div>
-            )}
-            <input
-              id="price"
-              placeholder="Enter your price"
-              type="integer"
-              value={values.price}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.price && touched.price
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-                        {errors.price && touched.price && (
-              <div className="input-feedback">{errors.price}</div>
-            )}
-            <input
-              id="location"
-              placeholder="Enter your location"
-              type="text"
-              value={values.location}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.location && touched.location
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-                        {errors.location && touched.location && (
-              <div className="input-feedback">{errors.location}</div>
-            )}
-            <input
-              id="size"
-              placeholder="Enter your size"
-              type="text"
-              value={values.size}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={
-                errors.size && touched.size
-                  ? "text-input error"
-                  : "text-input"
-              }
-            />
-            {errors.size && touched.size && (
-              <div className="input-feedback">{errors.size}</div>
-            )}
-
-            {/* <button
-              type="button"
-              className="outline"
-              onClick={handleReset}
-              disabled={!dirty || isSubmitting}
-            >
-              Reset
-            </button> */}
-            <button type="submit" onSubmit={handleSubmit} disabled={isSubmitting}>
-              Submit
-            </button>
-
-            {/* <DisplayFormikState {...props} /> */}
-          </form>
-        );
-      }}
-    </Formik>
-    {/* <form onSubmit={formik.handleSubmit}>
-      <label>Post Details</label>
-            <input 
-        id="title"
-        name="title"
-        onChange={formik.handleChange}
-        value={formik.values.title}
+      {/* <Modal show={editShow} onHide={handleEditClose}></Modal> */}
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+      <Form.Label>Title</Form.Label>
+      <Form.Control
+      autoFocus
+      name="title"
+      value={postState.title}
+      onChange={handleChange}
       />
-      <label>Post Details</label>
-      <input 
-        id="image"
-        name="image"
-        onChange={formik.handleChange}
-        value={formik.values.image}
+      </Form.Group>
+      <Form.Group>
+      <Form.Label>Image</Form.Label>
+      <Form.Control
+      name="image"
+      value={postState.image}
+      onChange={handleChange}
       />
-      <input 
-        id="price"
-        name="price"
-        onChange={formik.handleChange}
-        value={formik.values.price}
+      </Form.Group>
+       <Form.Group>
+      <Form.Label>Price</Form.Label>
+      <Form.Control
+      name="price"
+      value={postState.price}
+      onChange={handleChange}
       />
-      <input 
-        id="location"
-        name="location"
-        onChange={formik.handleChange}
-        value={formik.values.location}
+      </Form.Group>
+       <Form.Group>
+      <Form.Label>Location</Form.Label>
+      <Form.Control
+      name="location"
+      value={postState.location}
+      onChange={handleChange}
       />
-      <input 
-        id="size"
-        name="size"
-        onChange={formik.handleChange}
-        value={formik.values.size}
+      </Form.Group>
+      <Form.Group>
+      <Form.Label>Size</Form.Label>
+      <Form.Control
+      name="size"
+      value={postState.size}
+      onChange={handleChange}
       />
-      <button type="submit">Submit</button>
-    
-    </form> */}
-
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
     </>
   )
-  //Add Post fields here to create state
-};
+}
+
+
 
 export default PostForm;
